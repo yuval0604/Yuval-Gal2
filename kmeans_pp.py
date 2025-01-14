767d6f9b624c022c80641fd7b75672c4848c8c63
yuval0604 <bukovsky@mail.tau.ac.il>
2025-01-14 13:07:25 +0200

    Update kmeans_pp.py

--- kmeans_pp.py
+++ kmeans_pp.py
@@ -3,49 +3,62 @@ import pandas as pd
 import sys
 import mykmeanssp
 
-if len(sys.argv) not in [5,6]:
+if len(sys.argv) not in [5, 6]:
     print("An Error Has Occurred")
     sys.exit()
-    
 
 try:
-    k = sys.argv[1]
+    k = int(sys.argv[1])
     
-    if len(sys.argv)==5:
+    if len(sys.argv) == 5:
         max_iter = 300
-        eps = sys.argv[2]
+        eps = float(sys.argv[2])
         file_1 = sys.argv[3]
         file_2 = sys.argv[4]
-    
     else:
-        max_iter = sys.argv[2]
-        eps = sys.argv[3]
+        max_iter = int(sys.argv[2])
+        eps = float(sys.argv[3])
         file_1 = sys.argv[4]
         file_2 = sys.argv[5]
 
+    
     f1 = file_1[-4:]
     f2 = file_2[-4:]
-    if (f1 != ".txt" and f1 != ".csv") or (f2 != ".txt" and f2 != ".csv"):
+    if (f1 not in [".txt", ".csv"]) or (f2 not in [".txt", ".csv"]):
         print("NA")
-        sys.exit() 
+        sys.exit()
     
-    if max_iter.isdigit() == False or int(max_iter) <= 1 or int(max_iter) >= 1000 or max_iter[0]=="0":
-        print ("Invalid maximum iteration!")
-        sys.exit() 
+    if not (1 < max_iter < 1000):
+        print("Invalid maximum iteration!")
+        sys.exit()
     
-    if float(eps) < 0:
-        print ("Invalid epsilon!")
-        sys.exit()  
+    if eps < 0:
+        print("Invalid epsilon!")
+        sys.exit()
 
 except ValueError:
     print("An Error Has Occurred")
     sys.exit()
 
-df1 = pd.read_csv(file_1)
-df2 = pd.read_csv(file_2)
 
-df = pd.merge(df1, df2, on=df1.columns[0]).sort_values(by=df1.columns[0])
+df1 = pd.read_csv(file_1, header=None)
+df2 = pd.read_csv(file_2, header=None)
+
+
+key_column = 0  
+df1[key_column] = df1[key_column].astype(int)
+df2[key_column] = df2[key_column].astype(int)
+
+
+df = pd.merge(df1, df2, on=key_column, how='inner').sort_values(by=key_column)
+
+if df.empty:
+    print("An Error Has Occurred")
+    sys.exit()
+
+
 points = df.iloc[:, 1:].values
+
 
 np.random.seed(1234)
 initial_centroids_idx = [np.random.choice(range(points.shape[0]))]
@@ -55,10 +68,11 @@ for _ in range(1, k):
     new_centroid = np.random.choice(range(points.shape[0]), p=prob)
     initial_centroids_idx.append(new_centroid)
 
+
 initial_centroids = points[initial_centroids_idx]
 final_centroids = mykmeanssp.fit(initial_centroids.tolist(), points.tolist(), k, max_iter, eps)
+
 
 print(','.join(map(str, initial_centroids_idx)))
 for centroid in final_centroids:
     print(','.join(f"{coord:.4f}" for coord in centroid))
-
